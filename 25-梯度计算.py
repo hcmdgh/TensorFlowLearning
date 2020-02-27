a4b8027722b6520d1243458c50975795dad107d5
hcmdgh <hcmdgh@qq.com>
2020-02-27 13:03:04 +0800

    updated

--- 25-梯度计算.py
+++ 25-梯度计算.py
@@ -1,5 +1,8 @@
 from imports import *
 
+# 常见函数的梯度，参见：
+# assets/常见函数的梯度/
+
 # 1. 自动计算梯度
 
 # 1.1 tape.watch()
@@ -35,3 +38,29 @@ with tf.GradientTape() as tape:
 grad = tape.gradient(y, [x])
 print(grad)
 ''' [<tf.Tensor: shape=(), dtype=float32, numpy=108.0>] '''
+
+# 2. MSE梯度计算
+
+x = tf.random.normal([2, 4])
+w = tf.random.normal([4, 3])
+b = tf.zeros([3])
+y = tf.constant([2, 0])
+
+with tf.GradientTape() as tape:
+    tape.watch([w, b])
+
+    # prob_shape: [2, 3]
+    # y_onehot_shape: [2, 3]
+    prob = tf.nn.softmax(x @ w + b, axis=1)
+    loss = tf.reduce_mean(tf.losses.MSE(tf.one_hot(y, depth=3), prob))
+
+grads = tape.gradient(loss, [w, b])
+print(grads[0])
+''' tf.Tensor(
+[[-0.04857158  0.0378082   0.0107634 ]
+ [-0.03037842  0.00383345  0.02654496]
+ [ 0.03670761 -0.03769311  0.00098549]
+ [ 0.00831253 -0.0252247   0.01691214]], shape=(4, 3), dtype=float32) '''
+
+print(grads[1])
+''' tf.Tensor([-0.09032173  0.05539995  0.0349218 ], shape=(3,), dtype=float32) '''
